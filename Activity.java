530fd65221ac34ffec71c2fca35cbc239a859367
NicollMitchManingat <nicollmitch@gmail.com>
2024-09-12 15:04:46 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,9 @@
+class Activity{
+    public static void main(String args[]){
+        
+        Dog objDog = new Dog();
+
+        objDog.name = "Batuta";
+
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    String name;
+    boolean isMale;
+    int numberofpuppies;
+    Dog[] Puppies = new Dog[10];
+    Dog Name;
+
+    public String[] showName(){
+        return Name;
+    }
+    
+    public int setNumOfPuppies(){
+        return numberofpuppies;
+    }
+
+    public void showNumOfPuppies(){
+        System.out.println(numberofpuppies);
+    }
+}
