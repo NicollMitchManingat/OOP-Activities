32deb890b4e3b0a14908734876209c66acdabde8
NicollMitchManingat <nicollmitch@gmail.com>
2024-09-19 22:21:08 +0800

    Activity.java

--- Activity.java
+++ Activity.java
@@ -1,9 +1,22 @@
-class Activity{
-    public static void main(String args[]){
+public class Activity{
+    public static void main(String[] args){
         
-        Dog objDog = new Dog();
+        Dog dog = new Dog();
+        dog.name = "Batuta";
+        dog.puppies = 5;
 
-        objDog.name = "Batuta";
-
+        dog.dogs[0] = "Juan";
+        dog.dogs[1] = "Dos";
+        dog.dogs[2] = "Teres";
+        dog.dogs[3] = "Cuatro";
+        dog.dogs[4] = "Cinco";
+        
+        System.out.println(dog.dogName());
+        System.out.println("Woof! Woof!");
+        System.out.println(dog.numOfPuppies());
+        
+        for (int i = 0; i < 5; i++){
+            System.out.println(dog.dogs[i]);
+        }
     }
 }
