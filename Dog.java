93edbf78954dcb62849c62336f2a8ffc38c8d2d4
NicollMitchManingat <nicollmitch@gmail.com>
2024-09-20 09:25:17 +0800

    Dog.java

--- Dog.java
+++ Dog.java
@@ -1,14 +1,42 @@
 public class Dog{
     String name;
     boolean isMale;
-    int puppies;
-    String [] dogs = new String[5];
-    
-    public String dogName(){
+    int numberOfPuppies;
+    Dog[] puppies;
+
+    public Dog(String name){
+        this.name = name;
+        this.numberOfPuppies = 0;
+        puppies = new Dog[0];
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String showName(){
         return name;
     }
 
-    public int numOfPuppies(){
-        return puppies;
+    public void bark(){
+        System.out.println("Woof! Woof!");
+    }
+
+    public void setNumberOfPuppies(int numberOfPuppies){
+        this.numberOfPuppies = numberOfPuppies;
+    }
+
+    public int showNumberOfPuppies(){
+        return numberOfPuppies;
+    }
+
+    public void setPuppies(Dog[] puppies){
+        this.puppies = puppies;
+    }
+
+    public void showPuppies(){
+        for (Dog puppy : puppies) {
+            System.out.println(puppy.showName());
+        }
     }
 }
